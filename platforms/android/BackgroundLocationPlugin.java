5d3f95db4d31336d4a1303a45c3609dc72f7014a
idoivri <idoivri@gmail.com>
2015-07-11 17:53:11 +0300

    initial commit

---
+++ platforms/android/BackgroundLocationPlugin.java
@@ -0,0 +1,18 @@
+package com.zencity.cordova.backgroundlocation;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * BackgroundLocationPlugin is used for sending user location in the background.
+ *
+ */
+public class BackgroundLocationPlugin extends CordovaPlugin {
+	private static final String TAG = "BackgroundLocationPlugin"; //BackgroundGpsPlugin
+
+
+
+}
